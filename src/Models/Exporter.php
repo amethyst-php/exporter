ffc2d14af00ea96e09146c4f056dec0ccbaa31be
railken <railken@protonmail.ch>
2019-05-11 16:53:14 +0200

    fix relationships

--- src/Models/Exporter.php
+++ src/Models/Exporter.php
@@ -36,6 +36,6 @@ class Exporter extends Model implements EntityContract
      */
     public function data_builder(): BelongsTo
     {
-        return $this->belongsTo(DataBuilder::class);
+        return $this->belongsTo(config('amethyst.data-builder.data.data-builder.model'));
     }
 }
